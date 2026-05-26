4f9353d8bb24b8dfc0a21e80c2fd79b27f028bab
danielaherrera22345 <danielaherrera22345@gmail.com>
2026-05-26 11:59:09 -0500

    corrige auth login api render

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, tap, map } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private apiUrl = 'http://127.0.0.1:8000';
+  private apiUrl = 'https://api-techstore.onrender.com';
 
   constructor(private http: HttpClient) {}
 
   login(username: string, password: string): Observable<boolean> {
     return this.http.post<any>(
-      `${this.apiUrl}/login?username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`,
+      `${this.apiUrl}/login?username=${username}&password=${password}`,
       {}
     ).pipe(
       tap(response => {
@@ -25,15 +25,7 @@ export class AuthService {
     );
   }
 
-  logout(): void {
+  logout() {
     localStorage.removeItem('token');
-  }
-
-  isLoggedIn(): boolean {
-    return !!localStorage.getItem('token');
-  }
-
-  getToken(): string | null {
-    return localStorage.getItem('token');
   }
 }
